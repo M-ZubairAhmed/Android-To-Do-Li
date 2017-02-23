20fc2b2de410c5228f997a80e6e643030d1f1c51
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-02-23 19:18:38 +0530

    completed custom alert dialog for adding new task

--- app/src/main/java/com/fcchyd/todoli/UIActivity.java
+++ app/src/main/java/com/fcchyd/todoli/UIActivity.java
@@ -1,9 +1,7 @@
 package com.fcchyd.todoli;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
