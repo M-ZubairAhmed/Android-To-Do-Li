59f1c147db20c523a876b983f6bde128c3f7a6d3
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-02-24 14:40:45 +0530

    add new task alert update
    add new task alert dialog now stores the string value recieved from edit
    text

--- app/src/main/java/com/fcchyd/todoli/UIActivity.java
+++ app/src/main/java/com/fcchyd/todoli/UIActivity.java
@@ -1,5 +1,8 @@
 package com.fcchyd.todoli;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -7,10 +10,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class UIActivity extends AppCompatActivity {
 
     int undoneTaskCount = 5;
+    protected String newtask_String;
+    TextView somerandomText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,6 @@ public class UIActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,20 +34,32 @@ public class UIActivity extends AppCompatActivity {
                 createNewTask();
             }
         });
-
         TaskCounter(undoneTaskCount);
     }
 
+    protected void addList() {
+        somerandomText = (TextView) findViewById(R.id.textcomebackhere);
+        somerandomText.setText(newtask_String);
+    }
+
     protected void createNewTask(){
-        AlertDialog.Builder newTaskBuilder = new AlertDialog.Builder(this);
-
-        LayoutInflater inflater = this.getLayoutInflater();
-        newTaskBuilder.setView(inflater.inflate(R.layout.add_todo,null));
-
-        // create alert dialog
-        AlertDialog alertDialog = newTaskBuilder.create();
-
-        // show it
+        LayoutInflater inflater = LayoutInflater.from(UIActivity.this);
+        final View customView = inflater.inflate(R.layout.add_todo_alertdia,null);
+        final TextView addNewTask_Edittext = (EditText) customView.findViewById(R.id.alertdia_newtask_edittextxml);
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setView(customView)
+                .setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        newtask_String = addNewTask_Edittext.getText().toString();
+                        newtask_String = newtask_String.trim();
+                        if (newtask_String.length() != 0){
+                            Toast.makeText(UIActivity.this,"New task added",Toast.LENGTH_LONG).show();
+                            addList();
+                        }
+                    }
+                })
+                .create();
         alertDialog.show();
     }
 
@@ -55,5 +74,6 @@ public class UIActivity extends AppCompatActivity {
             this.setTitle(undoneTaskCount + " Pending Tasks");
         }
     }
+
 
 }
